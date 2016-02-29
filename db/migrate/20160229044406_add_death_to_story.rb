402c694894b29b05e946007c77496edca99b67e9
Zachary Levy <zacharyalevy@gmail.com>
2016-02-28 23:44:18 -0500

    death

---
+++ db/migrate/20160229044406_add_death_to_story.rb
@@ -0,0 +1,5 @@
+class AddDeathToStory < ActiveRecord::Migration[5.0]
+  def change
+    add_column :stories, :death, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229035352) do
+ActiveRecord::Schema.define(version: 20160229044406) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160229035352) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "ongoing"
+    t.boolean  "death"
   end
 
 end
